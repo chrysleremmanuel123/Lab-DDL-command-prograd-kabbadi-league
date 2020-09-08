8009a26c50d8b2a8475d16dd7c05d299dd2cb000
chrysleremmanuel123 <chrysler.emmanuel@gmail.com>
2020-09-08 12:58:11 +0530

    FSJ0820A017

--- ddl-pkl.sql
+++ ddl-pkl.sql
@@ -31,3 +31,51 @@
 -- 15. **Drop table skill**
 
 -- 16. **Drop table extra_type**
+create TABLE city
+(City_Name varchar(20)
+);
+create TABLE referee
+(referee_Name varchar(30)
+);
+create TABLE referee
+(referee_Name varchar(30)
+);
+create TABLE innings
+(Innings_Name varchar(30),
+Innings_Date date
+);
+create TABLE extra_type
+(extra_type_name varchar(30)
+);
+create TABLE skill
+(skill_name varchar(30)
+);
+create TABLE team
+(team_name varchar(30)
+);
+create TABLE player
+(player_name varchar(30)
+);
+create TABLE venue
+(venue_name varchar(30)
+);
+create TABLE event
+(event_name varchar(30)
+);
+create TABLE extra_event
+(extra_event_name varchar(30)
+);
+create TABLE outcome
+(outcome varchar(30)
+);
+create TABLE game
+(game_number number(3)
+);
+
+drop table city;
+
+drop table innings;
+
+drop table skill;
+
+drop table extra_type;
